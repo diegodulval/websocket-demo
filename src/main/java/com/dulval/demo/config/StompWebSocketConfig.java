b71b5b4b376c3e351bc3a4ea42be131d64b093a4
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-06-12 20:58:57 -0300

    Updated allowed origin

--- src/main/java/com/dulval/demo/config/StompWebSocketConfig.java
+++ src/main/java/com/dulval/demo/config/StompWebSocketConfig.java
@@ -13,13 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class StompWebSocketConfig implements WebSocketConfigurer {
-
+    
     private static final Logger logger = LoggerFactory.getLogger(StompWebSocketConfig.class);
-
+    
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/name");
+        registry.addHandler(new SocketHandler(), "/name").setAllowedOrigins("*");
         registry.addHandler(new ProtocolBufferSocketHandler(), "/protobuf");
         registry.addHandler(new BinarySocketHandler(), "/binary");
     }
-
+    
 }
